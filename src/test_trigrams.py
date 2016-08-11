89d9d9d852beae869186b1917aed83555262cfa4
zachrickert <zachrickert@gmail.com>
2016-08-10 17:32:02 -0700

    Add main function.

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -7,17 +7,37 @@ TEST_DICT = {('I', 'wish'): ["I", "I"],
     ('wish', 'I'): ["may", "might"],
     ('may', 'I'): ["wish"],
     ('I', 'may'): ["I"]}
+TEST_TUPLE = ('may', 'I')
 
 
 def test_get_input():
+    """Test to see if import corectly reads from a known file."""
     from trigrams import get_input
     assert get_input(TEST_FILE) == TEST_STRING
 
 
 def test_has_dictionary():
+    """Test to see if read_imput will return a dictionary."""
     from trigrams import read_input
     assert read_input("") == {}
 
+
 def test_dictionary_values():
+    """Test to see if read_input will currectly form a dictionary."""
     from trigrams import read_input
     assert read_input(TEST_STRING) == TEST_DICT
+
+
+def test_select_word():
+    """Test to see if pselect_word returns correct value."""
+    from trigrams import select_word
+    assert select_word(TEST_DICT, TEST_TUPLE) == 'wish'
+
+def test_start_of_story():
+    from trigrams import start_of_story
+    assert start_of_story(TEST_TUPLE) == 'may I'
+
+def test_add_to_story():
+    from trigrams import add_to_story
+    assert add_to_story(TEST_STRING, 'add') == 'I wish I may I wish I might add'
+
--- src/trigrams.py
+++ src/trigrams.py
@@ -1,21 +1,36 @@
 # -*- coding: utf-8 -*-
 import io
 
+from random import choice
+
 """Trigrams can be used to mutate text into new, surreal, forms."""
 
-def main():
-    pass
+def main(file_source, output_amt=200):
+    file_data = get_input(file_source)
+    trigram_dict = read_input(file_data)
+    current_key = choice(trigram_dict)
+    story = start_of_story(current_key)
+    story_count = 0
+    while True:
+        if story_count > output_amt or current_key not in trigram_dict:
+            break
+        next_word = select_word(trigram_dict, current_key)
+        add_to_story(story, next_word)
+        story_count += 1
+        current_key = (current_key[1], next_word)
+    print(story)
+
 
 def get_input(file_source):
     """Reads the data from a file."""
     f = io.open(file_source, encoding='utf-8')
     data = f.read()
     f.close()
-    print(data)
     return data
 
 
 def read_input(raw_data):
+    """Take raw data from a file and create a dictionary."""
     data = raw_data.split()
     trigram_dict = {}
 
@@ -29,10 +44,20 @@ def read_input(raw_data):
 
             trigram_dict[dict_key].append(dict_value)
 
-
     return trigram_dict
 
-# def select_word():
-#     pass
+
+def select_word(my_dict, my_key):
+    """Return a value from a dictionary."""
+    return choice(my_dict[my_key])
 
 
+def start_of_story(my_tuple):
+    return "{0} {1}".format(my_tuple[0], my_tuple[1])
+
+
+def add_to_story(story, new_word):
+    return "{} {}".format(story, new_word)
+
+if __name__ == '__main__':
+    main('../test.txt')
