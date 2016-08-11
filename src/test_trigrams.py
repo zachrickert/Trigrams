250e903f951b1987deb13f7ff92660e630f84bf9
zachrickert <zachrickert@gmail.com>
2016-08-11 08:49:51 -0700

    Change varible names for readibility.

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -18,14 +18,14 @@ def test_get_input():
 
 def test_has_dictionary():
     """Test to see if read_imput will return a dictionary."""
-    from trigrams import read_input
-    assert read_input("") == {}
+    from trigrams import create_trigram_dict
+    assert create_trigram_dict("") == {}
 
 
 def test_dictionary_values():
-    """Test to see if read_input will currectly form a dictionary."""
-    from trigrams import read_input
-    assert read_input(TEST_STRING) == TEST_DICT
+    """Test to see if create_trigram_dict will currectly form a dictionary."""
+    from trigrams import create_trigram_dict
+    assert create_trigram_dict(TEST_STRING) == TEST_DICT
 
 
 def test_select_word():
@@ -33,11 +33,19 @@ def test_select_word():
     from trigrams import select_word
     assert select_word(TEST_DICT, TEST_TUPLE) == 'wish'
 
+
+def test_select_random_start():
+    """Test to see if random start returns a key in the dictionary."""
+    from trigrams import select_random_start
+    assert select_random_start(TEST_DICT) in TEST_DICT
+
+
 def test_start_of_story():
     from trigrams import start_of_story
     assert start_of_story(TEST_TUPLE) == 'may I'
 
+
 def test_add_to_story():
     from trigrams import add_to_story
-    assert add_to_story(TEST_STRING, 'add') == 'I wish I may I wish I might add'
-
+    assert add_to_story(TEST_STRING, 'add') == (
+        'I wish I may I wish I might add')
--- src/trigrams.py
+++ src/trigrams.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
+"""Trigrams can be used to mutate text into new, surreal, forms."""
 import io
 
 from random import choice
 
-"""Trigrams can be used to mutate text into new, surreal, forms."""
 
 def main(file_source, output_amt=200):
     file_data = get_input(file_source)
-    trigram_dict = read_input(file_data)
-    current_key = choice(trigram_dict)
-    story = start_of_story(current_key)
-    story_count = 0
+    trigram_dict = create_trigram_dict(file_data)
+    current_phrase = select_random_start(trigram_dict)
+    story = start_of_story(current_phrase)
+    story_count = 2
     while True:
-        if story_count > output_amt or current_key not in trigram_dict:
+        if story_count > output_amt or current_phrase not in trigram_dict:
             break
-        next_word = select_word(trigram_dict, current_key)
+        next_word = select_word(trigram_dict, current_phrase)
         add_to_story(story, next_word)
         story_count += 1
-        current_key = (current_key[1], next_word)
+        current_phrase = (current_phrase[1], next_word)
     print(story)
 
 
 def get_input(file_source):
-    """Reads the data from a file."""
+    """Read data from a text file."""
     f = io.open(file_source, encoding='utf-8')
     data = f.read()
     f.close()
     return data
 
 
-def read_input(raw_data):
+def create_trigram_dict(raw_data):
     """Take raw data from a file and create a dictionary."""
     data = raw_data.split()
     trigram_dict = {}
@@ -47,16 +47,24 @@ def read_input(raw_data):
     return trigram_dict
 
 
+def select_random_start(my_dict):
+    """Return a random key from a dictionary."""
+    for key in my_dict:
+        return key
+
+
 def select_word(my_dict, my_key):
     """Return a value from a dictionary."""
     return choice(my_dict[my_key])
 
 
 def start_of_story(my_tuple):
+    """Change one tuple into a phrase."""
     return "{0} {1}".format(my_tuple[0], my_tuple[1])
 
 
 def add_to_story(story, new_word):
+    """Add a single word onto the end of the story."""
     return "{} {}".format(story, new_word)
 
 if __name__ == '__main__':
